778f4a148d2e746c4fef4c764c6e884a761d5919
Rachelforce <art.piskov@gmail.com>
2022-06-15 23:48:52 +0400

    Login register feature

---
+++ Assets/EcoTravel/User Managment/AuthenticateModel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AuthenticateModel : MonoBehaviour
+{
+    public string Login;
+    public string Password;
+}
